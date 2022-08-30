b8b38edac1d2569afd4b128cd4f1b57c0aaf16f3
tran phi anh <anhtpde140084@fpt.edu.vn>
2022-08-30 12:20:42 +0700

    update reward coin for miner

--- src/transaction.js
+++ src/transaction.js
@@ -1,122 +1,32 @@
-const SHA256 = require('crypto-js/sha256');
+const { Blockchain, Transaction } = require('./blockchain')
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction {
-  constructor(fromAddress, toAddress, amount) {
-    this.fromAddress = fromAddress;
-    this.toAddress = toAddress;
-    this.amount = amount;
-  }
-}
+const myKey = ec.keyFromPrivate('256ebe0c3cf356666523d3d3d39db1560ca6d223e04c4a2271dbe23c5a097d72');
+const myWalletAdress = myKey.getPublic('hex');
 
-class Block {
 
-  constructor(timestamp, transactions, previousHash = '') {
-    this.timestamp = timestamp;
-    this.transactions = transactions;
-    this.previousHash = previousHash;
-    this.hash = '';
-    this.nonce = 0;
-  }
+let savjeeCoin = new Blockchain();
 
-  // gen sha token
-  calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-  }
+// I want to say that create a transaction from my wallet to some address wwith 10 coin
+const tx1 = new Transaction(myWalletAdress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+savjeeCoin.addTransaction(tx1);
 
-  minBlock(difficulty) {
-    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1)
-      .join("0")) {
-      this.nonce++;
-      this.hash = this.calculateHash();
-    }
+// // create transaction
 
-    console.log("Block mined: ", this.hash);
-  }
-
-}
-
-class BlockChain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-    this.difficulty = 2;
-    this.pendingTransactions = [];
-    this.miningReward = 100;
-  }
-
-  createGenesisBlock() {
-    return new Block("01/01/2022", "Genesis Block", "0");
-  }
-
-  getLatesBlock() {
-    return this.chain[this.chain.length - 1];
-  }
-
-  minePendingTransaction(miningRewardAddress) {
-    let block = new Block(Date.now(), this.pendingTransactions);
-    block.minBlock(this.difficulty);
-
-    console.log("Block successfuly mined!");
-    this.chain.push(block);
-
-    this.pendingTransactions = [
-      new Transaction(null, miningRewardAddress, this.miningReward)
-    ]
-  }
-
-  createTransaction(transaction) {
-    this.pendingTransactions.push(transaction);
-  }
-
-  getBalanceOfAddress(address) {
-    let balance = 0;
-
-    for (const block of this.chain) {
-      for (const trans of block.transactions) {
-        if (trans.fromAddress === address) {
-          balance -= trans.amount;
-        }
-
-        if (trans.toAddress === address) {
-          balance += trans.amount;
-        }
-      }
-    }
-
-    return balance;
-  }
-
-  // check mắt xích có đúng không
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false;
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-}
-
-let savjeeCoin = new BlockChain();
-
-// create transaction
-
-savjeeCoin.createTransaction(new Transaction('address1', 'address2', 100));
-savjeeCoin.createTransaction(new Transaction('address2', 'address1', 50));
+// savjeeCoin.createTransaction(new Transaction('address1', 'address2', 100));
+// savjeeCoin.createTransaction(new Transaction('address2', 'address1', 50));
 
 console.log('\n Start the miner...');
-savjeeCoin.minePendingTransaction('anhtp-address');
+savjeeCoin.minePendingTransaction(myWalletAdress);
 
-console.log('\n Balance of anhTp is: ', savjeeCoin.getBalanceOfAddress('anhtp-address'));
+console.log('\n Balance of anhTp is: ', savjeeCoin.getBalanceOfAddress(myWalletAdress));
 
-console.log('\n Start the miner again...');
-savjeeCoin.minePendingTransaction('anhtp-address');
+// check is valid chain
+// savjeeCoin.chain[1].transactions[0].amount = 1;
 
-console.log('\n Balance of anhTp is: ', savjeeCoin.getBalanceOfAddress('anhtp-address'));
+// console.log('\n Start the miner again...');
+// savjeeCoin.minePendingTransaction('anhtp-address');
+
+// console.log('\n Balance of anhTp is: ', savjeeCoin.getBalanceOfAddress('anhtp-address'));
